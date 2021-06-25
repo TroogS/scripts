213dad4c12b6616e88162e1d26c5fafac4ac60c3
Marcel Fries <git@luxick.de>
2021-06-25 08:30:02 +0200

    Update esp-fixes.user.js

--- esp-fixes.user.js
+++ esp-fixes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ESP Fixes
 // @namespace    https://esp.eas-cpq.de/
-// @version      1.1
+// @version      1.2
 // @description  Collection of fixes for the EAS Service Portal
 // @updateURL    https://raw.githubusercontent.com/luxick/scripts/master/esp-fixes.user.js
 // @downloadURL  https://raw.githubusercontent.com/luxick/scripts/master/esp-fixes.user.js
