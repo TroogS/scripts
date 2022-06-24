9876caa45cce99177802f78b847478181a459695
luxick <git@luxick.de>
2022-06-24 09:28:11 +0200

    Create duckduckgo.user.js

---
+++ duckduckgo.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         DuckDuckGo Tab Title
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Sets the title of the tab when using the POST method for searching
+// @author       luxick
+// @match        https://duckduckgo.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=duckduckgo.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setTimeout(updateTitle, 1000);
+    // Your code here...
+})();
+
+function updateTitle(){
+    document.title = document.getElementById("search_form_input").value;
+}
